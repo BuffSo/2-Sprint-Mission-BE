5d39db828cd0b3a2588cc686ebc0b20fb496ded6
Jake So <buffso@naver.com>
2025-10-30 22:04:57 +0900

    chore: update mock user password to a1234567

--- prisma/mock.ts
+++ prisma/mock.ts
@@ -1,17 +1,18 @@
+// Mock users with password: a1234567
 export const mockUsers = [
   {
     email: 'user01@example.com',
     nickname: 'user01',
-    password: '$2b$10$Y7TYrNUSmGuByoFYa0pHC.qpNVU3/am/OpPMJrE1jgOIT9o9oxw4K',
+    password: '$2b$10$03DK1.bkaWLB3le.1u0KjOzVLlC4zwPXrDCkiYdIzaql.Jksgyyre',
   },
   {
     email: 'user02@example.com',
     nickname: 'user02',
-    password: '$2b$10$Y7TYrNUSmGuByoFYa0pHC.qpNVU3/am/OpPMJrE1jgOIT9o9oxw4K',
+    password: '$2b$10$03DK1.bkaWLB3le.1u0KjOzVLlC4zwPXrDCkiYdIzaql.Jksgyyre',
   },
   {
     email: 'user03@example.com',
     nickname: 'user03',
-    password: '$2b$10$Y7TYrNUSmGuByoFYa0pHC.qpNVU3/am/OpPMJrE1jgOIT9o9oxw4K',
+    password: '$2b$10$03DK1.bkaWLB3le.1u0KjOzVLlC4zwPXrDCkiYdIzaql.Jksgyyre',
   },
 ];
